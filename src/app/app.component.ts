a2ac28f9d1e630cff0bd642d735a63f632f1ebbc
Lucas <lucasax147@gmail.com>
2025-07-29 19:46:18 -0300

    Componente 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Componente01Component } from './componente01/componente01.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet, Componente01Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
