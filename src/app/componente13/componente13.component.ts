23d2805936e0fbb101f6f963316bf39b08d436b6
Lucas <lucasax147@gmail.com>
2025-08-18 20:23:58 -0300

    Criando formulario reativo

--- src/app/componente13/componente13.component.ts
+++ src/app/componente13/componente13.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Produto } from '../modelo/Produto';
 import { ProdutoService } from '../servico/produto.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-componente13',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './componente13.component.html',
   styleUrl: './componente13.component.css'
 })
@@ -17,7 +18,14 @@ export class Componente13Component {
 
   //visibilidade dos botoes
 
-  btnCadastrar: boolean = true
+  btnCadastrar: boolean = true;
+
+  //Objeto de formulario
+  formulario = new FormGroup({
+    id: new FormControl(null),
+    nome: new FormControl(''),
+    valor: new FormControl(null)
+  })
 
   //Construtor
 
