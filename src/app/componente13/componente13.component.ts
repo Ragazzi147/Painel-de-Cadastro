523906f18b904082ac16f1c8867c2f2f4bc8ae1f
Lucas <lucasax147@gmail.com>
2025-08-18 20:15:35 -0300

    Listagem de produtos

--- src/app/componente13/componente13.component.ts
+++ src/app/componente13/componente13.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Produto } from '../modelo/Produto';
+import { ProdutoService } from '../servico/produto.service';
 
 @Component({
   selector: 'app-componente13',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './componente13.component.html',
   styleUrl: './componente13.component.css'
 })
 export class Componente13Component {
 
+  // Vetor
+  vetor: Produto[] = [];
+
+  //Construtor
+
+  constructor(private servico: ProdutoService) { }
+
+  // Inicialização do componente
+
+  ngOnInit() {
+    this.selecionar();
+  }
+
+  //Metodo para selecionar todos os produtos
+  selecionar() {
+    this.servico.selecionar().subscribe(retorno => { this.vetor = retorno });
+  }
 }
