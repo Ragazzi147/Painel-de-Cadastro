5aad782e529b0eed02a3ed038c05fd7b31949f45
Lucas <lucasax147@gmail.com>
2025-08-21 11:22:37 -0300

    Redirecionamento de rota autenticada

--- src/app/autenticacao/rota.guard.ts
+++ src/app/autenticacao/rota.guard.ts
@@ -1,5 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const rotaGuard: CanActivateFn = (route, state) => {
-  return false;
+
+  //Constante
+  const rota = new Router;
+
+  //redirecionamento
+  if (localStorage.getItem('nome') === 'Ralf') {
+    return true;
+  } else {
+    rota.navigateByUrl('/pagina1')
+
+    return false;
+  }
+
 };
